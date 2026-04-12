9aa7821278f0644592652e0b545bcd53ab49f24f
konstantinvasilev <kosvasl09@gmail.com>
2026-04-12 23:20:07 +0900

    refactoring NewsListScreen.kt

--- app/src/main/java/com/example/newsapp/ui/screens/NewsListScreen.kt
+++ app/src/main/java/com/example/newsapp/ui/screens/NewsListScreen.kt
@@ -1,11 +1,16 @@
 package com.example.newsapp.ui.screens
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.newsapp.ui.components.ArticleCardCollapsed
 import com.example.newsapp.ui.models.ArticleUi
+import com.example.newsapp.ui.models.getMockArticleUiList
 import com.example.newsapp.ui.theme.NewsAppTheme
 
 @Composable
@@ -14,22 +19,31 @@ fun NewsListScreen(
     onCardClick: (ArticleUi) -> Unit,
     onShareClick: (ArticleUi) -> Unit,
     modifier: Modifier = Modifier,
-){
-    Column(
-        modifier = modifier
+) {
+    LazyColumn(
+        modifier = modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-        LazyColumn() {
-
+        items(
+            count = articleList.size,
+            key = { articleList[it].id },
+        ) {
+            ArticleCardCollapsed(
+                articleUi = articleList[it],
+                onCardClick = onCardClick,
+                onShareClick = onShareClick,
+            )
         }
     }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun NewsListScreenPreview(){
+fun NewsListScreenPreview() {
     NewsAppTheme() {
         NewsListScreen(
-            articleList = listOf(),
+            articleList = getMockArticleUiList(),
             onCardClick = {},
             onShareClick = {},
         )
