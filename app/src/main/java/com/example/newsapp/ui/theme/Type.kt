d6b33b4a8cd50324dd030777a519cce03d689f4d
konstantinvasilev <kosvasl09@gmail.com>
2026-04-12 22:30:04 +0900

    add new fonts

--- app/src/main/java/com/example/newsapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/newsapp/ui/theme/Type.kt
@@ -2,36 +2,49 @@ package com.example.newsapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.newsapp.R
+
+val MyCustomFontFamily = FontFamily(
+    Font(R.font.texturina_bold, FontWeight.Bold),
+    Font(R.font.roboto_medium, FontWeight.Medium)
+)
 
 val typography = Typography(
     headlineSmall = TextStyle(
-        fontWeight = FontWeight.SemiBold,
+        fontFamily = MyCustomFontFamily,
+        fontWeight = FontWeight.Bold,
         fontSize = 24.sp,
         lineHeight = 32.sp,
         letterSpacing = 0.sp
     ),
     titleLarge = TextStyle(
-        fontWeight = FontWeight.SemiBold,
+        fontFamily = MyCustomFontFamily,
+        fontWeight = FontWeight.Medium,
         fontSize = 18.sp,
         lineHeight = 32.sp,
         letterSpacing = 0.0.sp
     ),
     bodyLarge = TextStyle(
-        fontWeight = FontWeight.Normal,
+        fontFamily = MyCustomFontFamily,
+        fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.15.sp
     ),
     bodyMedium = TextStyle(
+        fontFamily = MyCustomFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.25.sp
     ),
     labelMedium = TextStyle(
-        fontWeight = FontWeight.Normal,
+        fontFamily = MyCustomFontFamily,
+        fontWeight = FontWeight.Medium,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
