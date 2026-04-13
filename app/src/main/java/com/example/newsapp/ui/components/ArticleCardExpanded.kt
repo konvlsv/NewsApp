6c41036e1946fb7964a4963c8ae3e017ef4c407d
konstantinvasilev <kosvasl09@gmail.com>
2026-04-13 09:50:49 +0900

    refactoring ArticleCardExpanded.kt

--- app/src/main/java/com/example/newsapp/ui/components/ArticleCardExpanded.kt
+++ app/src/main/java/com/example/newsapp/ui/components/ArticleCardExpanded.kt
@@ -1,21 +1,158 @@
 package com.example.newsapp.ui.components
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.newsapp.ui.models.ArticleUi
+import com.example.newsapp.ui.models.getMockArticleUiList
 import com.example.newsapp.ui.theme.NewsAppTheme
 
 @Composable
 fun ArticleCardExpanded(
+    articleUi: ArticleUi,
+    onCardClick: (ArticleUi) -> Unit,
+    onShareClick: (ArticleUi) -> Unit,
+    onOpenInBrowserClick: (ArticleUi) -> Unit,
     modifier: Modifier = Modifier,
-){
-
+) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        onClick = { onCardClick(articleUi) },
+        shape = RoundedCornerShape(8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+        ),
+        modifier = modifier.fillMaxWidth(),
+    ) {
+        Card(
+            elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+            onClick = { onCardClick(articleUi) },
+            shape = RoundedCornerShape(8.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surface,
+            ),
+            modifier = modifier.fillMaxWidth(),
+        ) {
+            Column() {
+                Box() {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text(
+                            text = articleUi.name,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                            style = MaterialTheme.typography.headlineSmall,
+                            color = MaterialTheme.colorScheme.onPrimaryContainer,
+                            modifier = Modifier.weight(1f),
+                        )
+                        IconButton(
+                            onClick = { onShareClick(articleUi) },
+                            modifier = Modifier.size(24.dp)
+                        ) {
+                            Icon(
+                                Icons.Default.Share,
+                                contentDescription = "Share",
+                            )
+                        }
+                    }
+                    Image(
+                        imageVector = Icons.Default.Person,
+                        contentDescription = "Article picture",
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        contentScale = ContentScale.Crop // Чтобы картинка не искажалась
+                    )
+                }
+                Text(
+                    text = articleUi.description,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.fillMaxWidth(),
+                )
+                Row(
+                    verticalAlignment = Alignment.Bottom,
+                    modifier = Modifier.padding(top = 4.dp)
+                ) {
+                    Text(
+                        text = articleUi.author,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        style = MaterialTheme.typography.labelMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        modifier = Modifier.weight(1f),
+                    )
+                    Text(
+                        text = articleUi.publishedAt,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        style = MaterialTheme.typography.labelMedium,
+                    )
+                }
+                Text(
+                    text = articleUi.content,
+                    maxLines = 4,
+                    overflow = TextOverflow.Ellipsis,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.fillMaxWidth(),
+                )
+                Button(
+                    onClick = { onOpenInBrowserClick(articleUi) }
+                ) {
+                    Text(
+                        text = "Open in browser",
+                        style = MaterialTheme.typography.labelMedium,
+                    )
+                }
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark",
+)
+@Preview(
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_NO,
+    name = "DefaultPreviewLight",
+)
 @Composable
-fun ArticleCardCollapsedExpanded(){
-    NewsAppTheme(){
-        ArticleCardExpanded()
+fun ArticleCardCollapsedExpanded() {
+    NewsAppTheme() {
+        ArticleCardExpanded(
+            articleUi = getMockArticleUiList().random(),
+            onCardClick = {},
+            onShareClick = {},
+            onOpenInBrowserClick = {},
+        )
     }
 }
