04a2227bec169ca541ccec8ce9af01315e101703
konstantinvasilev <kosvasl09@gmail.com>
2026-04-14 15:38:57 +0900

    add ArticleCategory.kt

--- app/src/main/java/com/example/newsapp/ui/models/ArticleCategory.kt
+++ app/src/main/java/com/example/newsapp/ui/models/ArticleCategory.kt
@@ -7,9 +7,9 @@ enum class ArticleCategory(
     val id: String,          // То, что отправляем в API
     @param:StringRes val label: Int // Ссылка на strings.xml
 ) {
+    GENERAL("general", R.string.general),
     BUSINESS("business", R.string.business),
     ENTERTAINMENT("entertainment", R.string.entertainment),
-    GENERAL("general", R.string.general),
     HEALTH("health", R.string.health),
     SCIENCE("science", R.string.science),
     SPORTS("sports", R.string.sports),
