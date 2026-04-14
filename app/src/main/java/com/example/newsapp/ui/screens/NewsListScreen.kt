a6a11a68ab33214534163733d7e12fe9bdea987e
konstantinvasilev <kosvasl09@gmail.com>
2026-04-14 20:20:54 +0900

    refactoring all NewsListScreen.kt

--- app/src/main/java/com/example/newsapp/ui/screens/NewsListScreen.kt
+++ app/src/main/java/com/example/newsapp/ui/screens/NewsListScreen.kt
@@ -2,23 +2,14 @@ package com.example.newsapp.ui.screens
 
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.newsapp.ui.components.ArticleCardCollapsed
-import com.example.newsapp.ui.components.ArticleCardExpanded
 import com.example.newsapp.ui.components.ArticleSearchBar
+import com.example.newsapp.ui.components.ArticlesLazyColumn
+import com.example.newsapp.ui.models.ArticleCategory
 import com.example.newsapp.ui.models.ArticleUi
 import com.example.newsapp.ui.models.getMockArticleUiList
 import com.example.newsapp.ui.theme.AppTheme
@@ -36,7 +27,10 @@ fun NewsListScreen(
         searchBarSearchText = "", //todo прокинуть во ViewModel
         searchBarOnValueChange = {}, //todo прокинуть во ViewModel
         searchBarOnDeleteClick = {}, //todo прокинуть во ViewModel
-        searchBarOnSearchClick = {} //todo прокинуть во ViewModel
+        searchBarOnSearchClick = {}, //todo прокинуть во ViewModel
+        articleSelectedCategory = ArticleCategory.GENERAL,
+        isCardExpanded = { false },
+        modifier = modifier
     )
 }
 
@@ -49,60 +43,29 @@ fun NewsListContent(
     searchBarOnValueChange: (String) -> Unit,
     searchBarOnDeleteClick: () -> Unit,
     searchBarOnSearchClick: () -> Unit,
+    articleSelectedCategory: ArticleCategory,
+    isCardExpanded: (ArticleUi) -> Boolean,
     modifier: Modifier = Modifier,
 ) {
-    // 1. Создаем состояние для хранения ID развернутых карточек
-    // Используем Set, чтобы можно было развернуть несколько карточек одновременно
-    var expandedCardIds by remember { mutableStateOf(setOf<String>()) }
-
-    LazyColumn(
-        modifier = modifier.fillMaxSize(),
-        contentPadding = PaddingValues(AppTheme.dimens.paddingExtraLarge),
-        verticalArrangement = Arrangement.spacedBy(AppTheme.dimens.lazyColumnItemsSpacing)
-    ) {
-        item() {
-            ArticleSearchBar(
-                searchText = searchBarSearchText,
-                onValueChange = searchBarOnValueChange,
-                onDeleteClick = searchBarOnDeleteClick,
-                onSearchClick = searchBarOnSearchClick
+    Column(modifier = modifier) {
+        ArticleSearchBar(
+            searchText = searchBarSearchText,
+            onValueChange = searchBarOnValueChange,
+            onDeleteClick = searchBarOnDeleteClick,
+            onSearchClick = searchBarOnSearchClick,
+            modifier = Modifier.padding(
+                horizontal = AppTheme.dimens.paddingSmall,
             )
-        }
-        // Используем items(articleList) вместо items(count), это удобнее
-        items(
-            items = articleList,
-            key = { it.id } // Ключ важен для правильной анимации списка
-        ) { article ->
-
-            // 2. Проверяем, развернута ли текущая карточка
-            val isExpanded = expandedCardIds.contains(article.id)
-
-            // 3. Добавляем анимацию смены размера
-            Box(
-                modifier = Modifier.animateContentSize()
-            ) {
-                if (isExpanded) {
-                    ArticleCardExpanded(
-                        articleUi = article,
-                        // При клике на развернутую — удаляем ID из списка (сворачиваем)
-                        onCardClick = {
-                            expandedCardIds = expandedCardIds - article.id
-                        },
-                        onShareClick = onShareClick,
-                        onOpenInBrowserClick = onOpenInBrowserClick
-                    )
-                } else {
-                    ArticleCardCollapsed(
-                        articleUi = article,
-                        // При клике на свернутую — добавляем ID в список (разворачиваем)
-                        onCardClick = {
-                            expandedCardIds = expandedCardIds + article.id
-                        },
-                        onShareClick = onShareClick,
-                    )
-                }
-            }
-        }
+        )
+        ArticlesLazyColumn(
+            articleList = articleList,
+            onShareClick = onShareClick,
+            onOpenInBrowserClick = onOpenInBrowserClick,
+            isCardExpanded = isCardExpanded,
+            articleSelectedCategory = articleSelectedCategory,
+            onArticleSelectedCategoryChange = {},
+            onExpandOrCollapseCardClick = {}
+        )
     }
 }
 
@@ -128,7 +91,9 @@ fun NewsListContentPreview() {
             searchBarSearchText = "",
             searchBarOnValueChange = {},
             searchBarOnDeleteClick = {},
-            searchBarOnSearchClick = {}
+            searchBarOnSearchClick = {},
+            articleSelectedCategory = ArticleCategory.GENERAL,
+            isCardExpanded = { false }
         )
     }
 }
