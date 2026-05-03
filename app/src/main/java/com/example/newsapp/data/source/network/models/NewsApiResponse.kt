85a29292b125407340aa3fae5db0976547294c4c
konstantinvasilev <kosvasl09@gmail.com>
2026-05-03 16:53:22 +0900

    change NewsApiResponse.kt

--- app/src/main/java/com/example/newsapp/data/source/network/models/NewsApiResponse.kt
+++ app/src/main/java/com/example/newsapp/data/source/network/models/NewsApiResponse.kt
@@ -1,7 +1,202 @@
 package com.example.newsapp.data.source.network.models
 
 data class NewsApiResponse(
-    val articles: List<Article>,
-    val status: String,
-    val totalResults: Int
+    val status: String?,
+    val totalResults: Int?,
+    val articles: List<Article>?
 )
+
+data class Article(
+    val source: Source?,
+    val author: String?,
+    val title: String?,
+    val description: String?,
+    val url: String?,
+    val urlToImage: String?,
+    val publishedAt: String?,
+    val content: String?
+)
+
+data class Source(
+    val id: String?,
+    val name: String?
+)
+
+fun getMockNewsApiResponse(): NewsApiResponse {
+    return NewsApiResponse(
+        status = "ok",
+        totalResults = 35,
+        articles = listOf(
+            Article(
+                source = Source(id = null, name = "TMZ"),
+                author = "TMZ Staff",
+                title = "'RHOBH' Kathy Hilton Fighting Injured House Guest Over \$\$\$ Demand - TMZ",
+                description = "'Real Housewives of Beverly Hills' star Kathy Hilton said the house guest suing her over an alleged fall at her Bel Air mansion may have caused her own injuries ... TMZ has learned.",
+                url = "https://www.tmz.com/2026/05/02/kathy-hilton-fighting-injured-house-guest-over-money/",
+                urlToImage = "https://imagez.tmz.com/image/d5/16by9/2026/05/01/d5f1f45da49046e2961f4e26efa60935_xl.png",
+                publishedAt = "2026-05-02T08:00:11Z",
+                content = "'Real Housewives of Beverly Hills' star Kathy Hilton said the house guest suing her over an alleged fall at her Bel Air mansion may have caused her own injuries ... TMZ has learned."
+            ),
+            Article(
+                source = Source(id = "the-washington-post", name = "The Washington Post"),
+                author = "Rachel Roubein, Praveena Somasundaram",
+                title = "Appeals court limits abortion pill access nationwide - The Washington Post",
+                description = "A federal appeals court issued a ruling that would temporarily block people from accessing abortion pills through telehealth providers and via mail.",
+                url = "https://www.washingtonpost.com/health/2026/05/01/abortion-pill-restrictions-appeals-court/",
+                urlToImage = "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/6OG7MHY4WBY423KVA76WAWCCY4_size-normalized.jpg&w=1440",
+                publishedAt = "2026-05-02T03:50:00Z",
+                content = "A federal appeals court is temporarily reinstating a requirement that abortion pills be picked up in person instead of sent through the mail."
+            ),
+            Article(
+                source = Source(id = "cbs-news", name = "CBS News"),
+                author = "Tucker Reals, Mark Osborne",
+                title = "Live Updates: Trump \"not satisfied\" with new peace deal offered by Iran - CBS News",
+                description = "Iran said it had offered a new proposal to the U.S. to end the war, as the Strait of Hormuz standoff sends costs soaring around the world.",
+                url = "https://www.cbsnews.com/live-updates/iran-war-trump-strait-of-hormuz-israel-lebanon-ceasefire/",
+                urlToImage = "https://assets3.cbsnewsstatic.com/hub/i/r/2026/05/01/b542b80e-785c-457d-8b40-1f36d7269927/thumbnail/1200x630/1cd9db5c1f7e2fd85b1c6277f6311896/tehran-2273253853.jpg",
+                publishedAt = "2026-05-02T02:49:00Z",
+                content = "The U.S. Embassy in Beirut has called on Lebanon's government to further its engagement with Israel."
+            ),
+            Article(
+                source = Source(id = null, name = "cleveland.com"),
+                author = "Chris Fedor, cleveland.com",
+                title = "Late Cleveland turnover, improbable 3-pointer send Raptors past Cavs 112-110 in overtime to force Game 7 - Cleveland.com",
+                description = "The Cavs were outlasted by the Raptors, 112-110, in overtime of Game 6 at Scotiabank Arena. The best-of-seven series is tied at 3-3, with the finale Sunday in Cleveland.",
+                url = "https://www.cleveland.com/cavs/2026/05/cavs-get-pushed-to-game-7-by-raptors-in-112-110-overtime-loss.html",
+                urlToImage = "https://www.cleveland.com/resizer/v2/ENU4PJ3VKRA6PL2VPMDWKHRLNQ.jpg",
+                publishedAt = "2026-05-02T02:44:00Z",
+                content = "The fourth-seeded Cavs were outlasted by the fifth-seeded Toronto Raptors, 112-110, in overtime of Game 6 on Friday night at Scotiabank Arena."
+            ),
+            Article(
+                source = Source(id = "the-washington-post", name = "The Washington Post"),
+                author = "Christopher Rowland",
+                title = "FDA expands access to a promising drug for one of the worst cancers - The Washington Post",
+                description = "More patients will be able to take the pill while it is under review, as the agency cited the dire need for a drug to fight one of the most lethal cancers.",
+                url = "https://www.washingtonpost.com/health/2026/05/01/pancreatic-cancer-fda-revolution-medicines/",
+                urlToImage = "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://cloudfront-us-east-1.images.arcpublishing.com/wapo/BNKRRMR2PWI5CIHCS3DUSJX7RM.JPG&w=1440",
+                publishedAt = "2026-05-02T02:41:26Z",
+                content = "The Food and Drug Administration said Friday that it has granted expanded access to a promising pancreatic cancer drug."
+            ),
+            Article(
+                source = Source(id = null, name = "WABC-TV"),
+                author = "Eyewitness News",
+                title = "NYC May Day: Protests and rallies take place across five boroughs - ABC7 New York",
+                description = "Hundreds of demonstrators poured into Washington Square Park in Greenwich Village determined to take a stand against large corporations and to stand up for organized labor.",
+                url = "https://abc7ny.com/post/nyc-may-day-rallies-planned-friday-protests-boroughs/19013672/",
+                urlToImage = "https://cdn.abcotvs.com/dip/images/19013780_may-day.jpg?w=1600",
+                publishedAt = "2026-05-02T01:53:16Z",
+                content = "Hundreds of demonstrators poured into Washington Square Park in Greenwich Village, determined to take a stand against large corporations and to stand up for organized labor."
+            ),
+            Article(
+                source = Source(id = null, name = "mlive.com"),
+                author = "Jacob Richman | jrichman@mlive.com",
+                title = "Pistons fight back from 24 down, force Game 7 vs. Magic with epic comeback - MLive.com",
+                description = "An all-time defensive showing in the second half opened the door for the Pistons to survive another elimination game.",
+                url = "https://www.mlive.com/pistons/2026/05/pistons-fight-back-from-24-down-force-game-7-vs-magic-with-epic-comeback.html",
+                urlToImage = "https://www.mlive.com/resizer/v2/LAW57TGF4ZHQTGCCGEUPVKACMM.jpg",
+                publishedAt = "2026-05-02T01:45:00Z",
+                content = "The Detroit Pistons aren't ready to call it a season."
+            ),
+            Article(
+                source = Source(id = "the-washington-post", name = "The Washington Post"),
+                author = "Salvador Rizzo, Samuel Oakford",
+                title = "Pirro says video shows moment gunman fired outside correspondents' dinner - The Washington Post",
+                description = "Defense attorneys for the alleged gunman have questioned the evidence in the case.",
+                url = "https://www.washingtonpost.com/national-security/2026/05/01/coorespondents-dinner-shotgun-evidence/",
+                urlToImage = "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://cloudfront-us-east-1.images.arcpublishing.com/wapo/2NIPI5WFLME762OKN7F3HL6IYU.JPG&w=1440",
+                publishedAt = "2026-05-02T01:22:28Z",
+                content = "U.S. Attorney Jeanine Pirro posted video to social media that she said shows the alleged assailant at the White House correspondents dinner."
+            ),
+            Article(
+                source = Source(id = "the-wall-street-journal", name = "The Wall Street Journal"),
+                author = "WSJ",
+                title = "Trump Orders the Withdrawal of 5,000 U.S. Troops From Germany - WSJ",
+                description = null,
+                url = "https://www.wsj.com/world/europe/trump-orders-the-withdrawal-of-5-000-u-s-troops-from-germany-e6e7ca87",
+                urlToImage = null,
+                publishedAt = "2026-05-02T01:02:00Z",
+                content = null
+            ),
+            Article(
+                source = Source(id = null, name = "Deadline"),
+                author = "Glenn Garner",
+                title = "Jodie Sweetin Received One-Cent Residual Check For 'Full House' - Deadline",
+                description = "How rude! Jodie Sweetin's latest residual check from 'Full House' sure isn't buying her a San Francisco townhouse anytime soon.",
+                url = "http://deadline.com/2026/05/jodie-sweetin-one-cent-residual-check-full-house-1236878822/",
+                urlToImage = "https://deadline.com/wp-content/uploads/2026/05/P1149FVZ-e1777682632752.jpg?w=1024",
+                publishedAt = "2026-05-02T01:02:00Z",
+                content = "Jodie Sweetin recently revealed that she got a one-cent check the other day for Full House residuals."
+            ),
+            Article(
+                source = Source(id = "politico", name = "Politico"),
+                author = "Kimberly Leonard",
+                title = "'What's a more secure place than The Villages?': Trump finds his people again in Florida - Politico",
+                description = "The event had the feel of a pep rally for the president at a time when his popularity is flagging, even in Florida.",
+                url = "https://www.politico.com/news/2026/05/01/florida-trump-villages-speech-economy-00903744",
+                urlToImage = "https://www.politico.com/dims4/default/resize/1200/quality/90/format/jpg?url=https%3A%2F%2Fstatic.politico.com%2F51%2Fb3%2F498cba394e17af5406e469f0c39e%2Ftrump-8427.jpg",
+                publishedAt = "2026-05-02T00:33:50Z",
+                content = "Trump met a roaring, supportive crowd with many still in line outside the charter school where the rally was held."
+            ),
+            Article(
+                source = Source(id = "cbs-news", name = "CBS News"),
+                author = "Joe Walsh",
+                title = "Tennessee and Alabama take steps to redraw House maps in wake of Supreme Court ruling - CBS News",
+                description = "The Republican governors of Tennessee and Alabama called state lawmakers into special sessions on Friday.",
+                url = "https://www.cbsnews.com/news/tennessee-alabama-take-steps-to-redraw-house-maps-supreme-court-ruling-redistricting/",
+                urlToImage = "https://assets1.cbsnewsstatic.com/hub/i/r/2026/04/16/a8309647-304a-4580-ba8b-15306c4e2a3a/thumbnail/1200x630/6ee679c6564b419bb95af20d9c2f56fa/cbsn-fusion-looking-beyond-supreme-court-6-3-ideological-split-thumbnail.jpg",
+                publishedAt = "2026-05-02T00:31:00Z",
+                content = "The Republican governors of Tennessee and Alabama called state lawmakers into special sessions on Friday."
+            ),
+            Article(
+                source = Source(id = null, name = "The Advocate"),
+                author = "Will Nickel",
+                title = "Post Malone and Jelly Roll's Tiger Stadium concert canceled - The Advocate",
+                description = "The two singers were scheduled to perform at Tiger Stadium on May 23.",
+                url = "https://www.theadvocate.com/baton_rouge/entertainment_life/post-malone-jelly-roll-tiger-stadium-canceled/article_1d8b5bf3-e322-46fb-ad0a-32ec251a4733.html",
+                urlToImage = "https://bloximages.newyork1.vip.townnews.com/theadvocate.com/content/tncms/assets/v3/editorial/0/4e/04ec798e-e992-52d2-9230-f6677c223aa3/69820c542892e.image.jpg",
+                publishedAt = "2026-05-02T00:23:00Z",
+                content = "Multi-platinum artist Post Malone and three-time Grammy winner Jelly Roll announced that their planned Tiger Stadium concert is canceled."
+            ),
+            Article(
+                source = Source(id = "cnn", name = "CNN"),
+                author = "Marnie Hunter",
+                title = "What passengers need to know about Spirit Airlines - CNN",
+                description = "Thousands of Spirit Airlines passengers are facing a chaotic weekend as the budget carrier announced it's halting all flights as of early Saturday.",
+                url = "https://www.cnn.com/2026/05/01/travel/passengers-need-to-know-spirit-airlines",
+                urlToImage = "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2265835396.jpg?c=16x9&q=w_800,c_fill",
+                publishedAt = "2026-05-02T00:20:00Z",
+                content = "Thousands of Spirit Airlines passengers are facing a chaotic weekend as the budget carrier announced its halting all flights as of early Saturday."
+            ),
+            Article(
+                source = Source(id = null, name = "CNET"),
+                author = "See full bio",
+                title = "Google Photos' New AI Tool Will Help You Picture Yourself in All Your Clothes - CNET",
+                description = "The Google Photos wardrobe feature uses AI to scan your camera roll and create a digital version of your closet.",
+                url = "https://www.cnet.com/tech/services-and-software/google-photos-wardrobe-ai-try-on-feature/",
+                urlToImage = "https://www.cnet.com/a/img/resize/852a92792d1e2a35d9483043cfc150bc63d087cd/hub/2026/05/01/3fc24634-0fc6-4477-9a6e-b1b1270c58c4/screenshot-2026-05-01-at-12-48-31pm.png",
+                publishedAt = "2026-05-01T22:53:00Z",
+                content = "Google Photos is launching a new AI wardrobe feature that scans your camera roll and creates a digital version of your closet."
+            ),
+            Article(
+                source = Source(id = null, name = "SpaceNews"),
+                author = "Jeff Foust",
+                title = "NASA to increase value of CLPS contract to support surge of lunar lander missions - SpaceNews",
+                description = "NASA is planning to increase the total value of a contract for robotic lunar lander missions to support a proposed surge in flights for a moon base.",
+                url = "http://spacenews.com/nasa-to-increase-value-of-clps-contract-to-support-surge-of-lunar-lander-missions/",
+                urlToImage = "https://spacenews.com/wp-content/uploads/2026/03/im-5.jpeg",
+                publishedAt = "2026-05-01T22:43:05Z",
+                content = "NASA is planning to increase the total value of a contract for robotic lunar lander missions to support a proposed surge in flights for the agency's moon base plans."
+            ),
+            Article(
+                source = Source(id = "ars-technica", name = "Ars Technica"),
+                author = "Kyle Orland",
+                title = "Study: AI models that consider user's feeling are more likely to make errors - Ars Technica",
+                description = "Overtuning can cause models to prioritize user satisfaction over truthfulness.",
+                url = "https://arstechnica.com/ai/2026/05/study-ai-models-that-consider-users-feeling-are-more-likely-to-make-errors/",
+                urlToImage = "https://cdn.arstechnica.net/wp-content/uploads/2026/05/GettyImages-1338190481-1152x648.jpg",
+                publishedAt = "2026-05-01T22:23:36Z",
+                content = null
+            )
+        )
+    )
+}
--- app/src/main/java/com/example/newsapp/data/source/network/models/Source.kt
+++
@@ -1,6 +0,0 @@
-package com.example.newsapp.data.source.network.models
-
-data class Source(
-    val id: String,
-    val name: String
-)
